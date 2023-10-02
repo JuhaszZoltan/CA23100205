7feb1b10e91d1b5507d6f43fbfcd86455d499f3c
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-10-02 13:05:57 +0200

    add cat handling

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace CA23100205
         {
             string name = "Zoli";
             Console.WriteLine($"Hello, {name}!");
+            Console.WriteLine($"{name}, do you like cats?");
+            Console.Write(">>>  ");
+            string resp = Console.ReadLine();
+            if (resp.ToLower().StartsWith("yes"))
+            {
+                Console.WriteLine("awwwww :3 :3 :3");
+            }
+            else Console.WriteLine(":(((((");
         }
     }
 }
