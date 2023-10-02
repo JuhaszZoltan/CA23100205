83ee0166e813476906b72b1481fe684e47dcdb7a
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2023-10-02 13:28:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,15 @@ namespace CA23100205
                 Console.WriteLine("awwwww :3 :3 :3");
             }
             else Console.WriteLine(":(((((");
+
+            Console.WriteLine("What is your favorite color?");
+            Console.Write(">>>  ");
+            resp = Console.ReadLine();
+            if (resp.ToLower().Contains("blue"))
+            {
+                Console.ForegroundColor = ConsolColor.Blue;
+                Console.Writeline("This?")
+            }
         }
     }
 }
