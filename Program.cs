6b885bd36232e32dd5daae00bfab61ba82c61f10
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-10-02 12:49:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CA23100205
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name = "Zoli";
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
